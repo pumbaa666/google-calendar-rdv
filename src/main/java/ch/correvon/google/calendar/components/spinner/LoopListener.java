fcd45284587b99b3a21b893ae2e71ca4b41310ce
loic-pryv <loic@pryv.com>
2021-01-07 14:10:38 +0100

    1st cleanup

--- src/main/java/ch/correvon/google/calendar/components/spinner/LoopListener.java
+++
@@ -1,6 +0,0 @@
-package ch.correvon.google.calendar.components.spinner;
-
-public interface LoopListener
-{
-	public void looped(int sens);
-}
--- src/main/java/ch/correvon/google/calendar/window/mainWindow/jtable/ColorableModel.java
+++
@@ -1,8 +0,0 @@
-package ch.correvon.google.calendar.window.mainWindow.jtable;
-
-import java.awt.Color;
-
-public interface ColorableModel
-{
-	public Color getColor(int row);
-}
